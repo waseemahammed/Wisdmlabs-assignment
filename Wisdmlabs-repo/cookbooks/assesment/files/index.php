bb13de613d0731bc64655ae603e5390d8d7aef06
Waseem Ahammed <waseemahammed96@gmail.com>
2018-01-29 12:56:51 +0530

    Update index.php

--- Wisdmlabs-repo/cookbooks/assesment/files/index.php
+++ Wisdmlabs-repo/cookbooks/assesment/files/index.php
@@ -7,8 +7,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=counter", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-	// gets the user IP Address
+
 	$user_ip=$_SERVER['REMOTE_ADDR'];
 	$sql ="select userip from pageview where page='52.66.11.183' and userip='$user_ip'";
 	$insert = "insert into pageview(page,userip) values('52.66.11.183','$user_ip')";
